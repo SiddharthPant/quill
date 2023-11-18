e48ab0c15bc35897e6a40a1192bae415ddb469cc
SiddharthPant <spsiddharthpant@gmail.com>
2023-11-19 03:56:03 +0530

    Implement empty user file list on dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,29 @@
+"use client";
+
+import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./UploadButton";
+import { Ghost } from "lucide-react";
 
 const Dashboard = () => {
+  const { data: files, isLoading } = trpc.getUserFiles.useQuery();
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
         <UploadButton />
       </div>
+      {/* display all user files */}
+      {files && files?.length !== 0 ? (
+        <div></div>
+      ) : isLoading ? (
+        <div></div>
+      ) : (
+        <div className="mt-16 flex flex-col items-center gap-2">
+          <Ghost className="h-8 w-8 text-zinc-800" />
+          <h3 className="font-semibold text-xl">Pretty empty around here</h3>
+          <p>Let&apos;s upload your first PDF.</p>
+        </div>
+      )}
     </main>
   );
 };
