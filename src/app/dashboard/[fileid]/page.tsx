d5eedd9d759f2b0fa867344947fccd7edb8ffdcd
SiddharthPant <spsiddharthpant@gmail.com>
2023-11-22 14:02:38 +0530

    fetch user file data on detail page

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,13 +1,29 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
 interface PageProps {
   params: {
     fileid: string;
   };
 }
 
-const Page = ({ params }: PageProps) => {
+const Page = async ({ params }: PageProps) => {
   // retrieve the file id
   const { fileid } = params;
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/$(fileid}`);
   // make the database call
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+
+  if (!file) notFound;
+
   return <div>{fileid}</div>;
 };
 
