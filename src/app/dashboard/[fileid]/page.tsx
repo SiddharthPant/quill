38bc8b37e5b43c0ddcdd933bb3521f03ec4e3417
SiddharthPant <spsiddharthpant@gmail.com>
2023-11-22 15:08:44 +0530

    Add ChatWrapper to pdf detail page

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,3 +1,4 @@
+import ChatWrapper from "@/components/ChatWrapper";
 import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
@@ -23,16 +24,19 @@ const Page = async ({ params }: PageProps) => {
     },
   });
 
-  if (!file) notFound;
+  if (!file) notFound();
 
   return (
-    <div className="flex1 justify-between flex flex-col h-[calc(100vh)-3.5rem]">
+    <div className="flex1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2 ">
         {/* left side */}
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
             <PdfRenderer />
           </div>
+        </div>
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0 ">
+          <ChatWrapper />
         </div>
       </div>
     </div>
