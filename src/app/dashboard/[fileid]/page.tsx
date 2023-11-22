8921d6e81c650c15dc7caedcd9fd0ddb7f65e848
SiddharthPant <spsiddharthpant@gmail.com>
2023-11-22 14:18:25 +0530

    Add PdfRenderer component on detail page

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,3 +1,4 @@
+import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
@@ -24,7 +25,18 @@ const Page = async ({ params }: PageProps) => {
 
   if (!file) notFound;
 
-  return <div>{fileid}</div>;
+  return (
+    <div className="flex1 justify-between flex flex-col h-[calc(100vh)-3.5rem]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2 ">
+        {/* left side */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
