ed9359b3bb1d64f77ba43b58d20b670d977ea578
SiddharthPant <spsiddharthpant@gmail.com>
2023-11-25 17:53:25 +0530

    Trigger fullscreen on click of icon

--- src/components/PdfFullscreen.tsx
+++ src/components/PdfFullscreen.tsx
@@ -26,7 +26,7 @@ const PdfFullscreen = ({ fileUrl }: PdfFullscreenProps) => {
         }
       }}
     >
-      <DialogTrigger asChild>
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
         <Button variant="ghost" className="gap-1.5" aria-label="fullscreen">
           <Expand className="h-4 w-4" />
         </Button>
