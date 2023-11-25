7ac65884f9fe8b8d7ef607ca68aea439eedd68a6
SiddharthPant <spsiddharthpant@gmail.com>
2023-11-25 20:14:52 +0530

    Replace with TextAreaAutosixe in shadcn componnet

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,14 +1,17 @@
 import * as React from "react"
+import TextareaAutosize, {
+  TextareaAutosizeProps,
+} from "react-textarea-autosize";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaAutosizeProps>(
   ({ className, ...props }, ref) => {
     return (
-      <textarea
+      <TextareaAutosize
         className={cn(
           "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
@@ -16,9 +19,9 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
+);
 Textarea.displayName = "Textarea"
 
 export { Textarea }
