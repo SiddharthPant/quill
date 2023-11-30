0fc987dcfae794c9369b3dadff6a170a63190b67
SiddharthPant <spsiddharthpant@gmail.com>
2023-12-01 00:10:22 +0530

    Finish message component

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { ExtendedMessage } from "@/types/message";
+import { Icons } from "../Icons";
 
 interface MessageProps {
   message: ExtendedMessage;
@@ -7,7 +8,30 @@ interface MessageProps {
 }
 
 const Message = ({ message, isNextMessageSamePerson }: MessageProps) => {
-  return <div className={cn("flex items-end")}></div>;
+  return (
+    <div
+      className={cn("flex items-end", {
+        "justify-end": message.isUserMessage,
+      })}
+    >
+      <div
+        className={cn(
+          "realtive flex h-6 w-6 aspect-square items-center justify-center",
+          {
+            "order-2 bg-blue-600 rounded-sm": message.isUserMessage,
+            "order-1 bg-zinc-800 rounded-sm": !message.isUserMessage,
+            invisible: isNextMessageSamePerson,
+          }
+        )}
+      >
+        {message.isUserMessage ? (
+          <Icons.user className="fill-zinc-200 text-zinc-200 h-3/4 w-3/4" />
+        ) : (
+          <Icons.logo className="fill-zinc-300 h-3/4 w-3/4" />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Message;
