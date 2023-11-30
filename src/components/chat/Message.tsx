ec9b9644c856644f0bec4243f4bfa433eca7ca67
SiddharthPant <spsiddharthpant@gmail.com>
2023-11-30 23:56:32 +0530

    Add message types

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,5 +1,13 @@
-const Message = () => {
-  return <div></div>;
+import { cn } from "@/lib/utils";
+import { ExtendedMessage } from "@/types/message";
+
+interface MessageProps {
+  message: ExtendedMessage;
+  isNextMessageSamePerson: boolean;
+}
+
+const Message = ({ message, isNextMessageSamePerson }: MessageProps) => {
+  return <div className={cn("flex items-end")}></div>;
 };
 
 export default Message;
